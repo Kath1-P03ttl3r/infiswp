77bf51809bad3a4a81daf888b3d6ddff22101fc1
Kath1 P03ttl3r <POE230111@spengergasse.at>
2024-11-19 14:54:21 +0100

    i like to wear crocs but my parents wont let me wear them outside

--- 2024-11-12/idk.js
+++ 2024-11-12/idk.js
@@ -1,14 +1,16 @@
 
 
 const fileslocation = './nmap-datenfiles/';
-try{
-    const dirEntries = await Deno.readDir('./nmap-datenfiles');
-    for await (const dirEntry of dirEntries) {
+async function main() {
+    try{
+        const dirEntries = await Deno.readDir('./nmap-datenfiles');
+        for await (const dirEntry of dirEntries) {
         //console.log(dirEntry.name);
         const data = await Deno.readTextFile('./nmap-datenfiles/'+filePath);
         const lines = data.split('\n');
         console.log(lines);
-    }catch (err) {
+        }       
+     }catch (err) {
         console.error('Error reading the directory:', err);
     }
 }
