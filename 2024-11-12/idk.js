b3d5ee5b993d8a43b549e99047dcc27b5c3cb725
Kath1 P03ttl3r <POE230111@spengergasse.at>
2024-12-09 18:43:03 +0100

    ketg

--- 2024-11-12/idk.js
+++ 2024-11-12/idk.js
@@ -1,21 +1,12 @@
-
-
-const fileslocation = './nmap-datenfiles/';
+const filePath = './nmap-datenfiles';
 async function main() {
-    try{
-        const dirEntries = await Deno.readDir('./nmap-datenfiles');
+    try {
+        const dirEntries = await Deno.readDir(filePath);
         for await (const dirEntry of dirEntries) {
-        //console.log(dirEntry.name);
-        const data = await Deno.readTextFile('./nmap-datenfiles/'+filePath);
-        const lines = data.split('\n');
-        console.log(lines);
-        }       
-     }catch (err) {
+            console.log(dirEntry.name);
+        }
+    } catch (err) {
         console.error('Error reading the directory:', err);
     }
 }
 main();
-
-// nmap/ date csv oder so und nicht mehr umgeleitet
-//Deno.writeTextFile
-//sont vorgeschriebenes nehemen
