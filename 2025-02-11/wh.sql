eaccc5ec8d81fee5f514eb1429fd3263b492fd1c
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-02-11 14:02:12 +0100

    dingdong

---
+++ 2025-02-11/wh.sql
@@ -0,0 +1,10 @@
+/* sind Datenbankfiles
+
+1)  SQLite3 Editor verwendet SQL Bibliothek um auf die Files zuzugreifen
+
+2) sqlite3 ist ein Kommando
+also: strg + j --> terminal und dann sqlite3 eingeben
+
+
+Kommentar in sql_ --
+
