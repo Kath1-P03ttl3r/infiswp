14e6cfa975a62f3d5d11f2c6d9f48b05d0920f66
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-02-24 10:16:24 +0100

    ed

---
+++ 2025-02-18/176.sql
@@ -0,0 +1,10 @@
+176. Second Highest Salary
+
+WITH
+  RankedEmployees AS (
+    SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS `rank`
+    FROM Employee
+  )
+SELECT MAX(salary) AS SecondHighestSalary
+FROM RankedEmployees
+WHERE `rank` = 2;
