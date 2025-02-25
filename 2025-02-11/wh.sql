e23a6f9edfbfbe65acaaa8ff18af428fab064b11
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-02-25 15:10:56 +0100

    rg4

--- 2025-02-11/wh.sql
+++ 2025-02-11/wh.sql
@@ -8,3 +8,7 @@ also: strg + j --> terminal und dann sqlite3 eingeben
 
 Kommentar in sql_ --
 
+neue tabelle mit gleichem inhalt
+im terminal: sqlite> 
+create table -2 as select * from -;
+select count() from -2
