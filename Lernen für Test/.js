c8e47f4cab411dcd59fdc40b6be2d2b04acbc4b1
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-03-09 15:12:27 +0100

    re

--- Lernen für Test/.js
+++ Lernen für Test/.js
@@ -4,7 +4,12 @@ function sayHi() {
     //we can have several lines here
 }
 //function saves code but doesnt do anything until called
-
 //calling the function:
 sayHi()
 //says 'Hi'
+
+function sum(a, b) {
+    console.log(a + b)
+}
+sum(1, 2)
+// equals 3, if we dont put a value, it says undefined
