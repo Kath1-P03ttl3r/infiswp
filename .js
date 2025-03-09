c76354c25e04a6d7e29c91090192ac8515aa826d
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-03-09 14:55:36 +0100

    wefgef

---
+++ .js
@@ -0,0 +1,10 @@
+function sayHi() {
+    //everything in here gets executed everytime i call the function
+    console.log('Hi')
+    //we can have several lines here
+}
+//function saves code but doesnt do anything until called
+
+//calling the function:
+sayHi()
+//says 'Hi'
