cb333b7befa4a8dce0916d6869c2fd333f9812fd
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-03-10 16:56:11 +0100

    w

---
+++ Lernen für Test/static/script.js
@@ -0,0 +1,35 @@
+const ul = document.querySelector("ul");
+
+const counterElement = document.querySelector("#counter");
+counterElement.onclick = generateList;
+
+const deleteElement = document.querySelector("#delete");
+deleteElement.onclick = deleteList;
+
+let x = 10;
+
+/*
+const listItems = document.querySelectorAll("li")
+console.log(listItems);
+const deleteAll = document.querySelector("#clear")
+deleteAll.addEventListener("click", function(){
+  liRemove;
+});
+*/
+function generateList() {
+  for (let i = 1; i <= x; i++) {
+    const li = document.createElement("li");
+    li.innerText = i;
+    ul.appendChild(li);
+  }
+}
+
+function deleteList() {
+  const liRemove = ul.lastElementChild;
+  if (liRemove) {
+    ul.removeChild(liRemove);
+  } else {
+    console.log("Hey, es gibt keine Listenelemente mehr!!!");
+    alert("Hey, es gibt keine Listenelemente mehr!!!")
+  }
+}
