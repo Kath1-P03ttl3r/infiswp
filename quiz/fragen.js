a3646a3be6929852a9a7d4db037d16d8a864c716
Kath1 P03ttl3r <POE230111@spengergasse.at>
2025-06-10 14:37:37 +0200

    g

--- quiz/fragen.js
+++ quiz/fragen.js
@@ -1,33 +1,33 @@
 export const fragen = [
     {
-        "frage": "",
+        "frage": "Who wrote the novel Pride and Prejudice?",
+        "optionen": ["Barbara Pym", "Jane Austen", "Robin McKinley", "Elizabeth Gaskell"],
+        "antwort": "Jane Austen"
+    },
+    {
+        "frage": "What is the name of the smallest country in the world?",
+        "optionen": ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
+        "antwort": "Vatican City"
+    },
+    {
+        "frage": "What is the sign directly opposite Scorpio in the zodiac?",
         "optionen": ["", "", "", ""],
         "antwort": ""
     },
     {
-        "frage": "",
-        "optionen": ["", "", "", ""],
-        "antwort": ""
+        "frage": "Which empire had no written language?",
+        "optionen": ["Incan", "Aztec", "Egyptian", "Roman"]
+        "antwort": "Incan"
     },
     {
-        "frage": "",
-        "optionen": ["", "", "", ""],
-        "antwort": ""
+        "frage": "How many stripes are there on the US flag?",
+        "optionen": ["11", "15", "12", "13"],
+        "antwort": "13"
     },
     {
-        "frage": "",
-        "optionen": ["", "", "", ""],
-        "antwort": ""
-    },
-    {
-        "frage": "",
-        "optionen": ["", "", "", ""],
-        "antwort": ""
-    },
-    {
-        "frage": "",
-        "optionen": ["", "", "", ""],
-        "antwort": ""
+        "frage": "How many time zones are there in Russia?",
+        "optionen": ["11", "15", "12", "13"],
+        "antwort": "11"
     },
     {
         "frage": "",
